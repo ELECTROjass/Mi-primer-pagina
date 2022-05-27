0973e4441b901add47634c7f51cb320904dbf759
ELECTROjass <106362554+ELECTROjass@users.noreply.github.com>
2022-05-26 22:30:54 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.nav-main');
+ScrollReveal().reveal('.showcase1', { delay:500});
+ScrollReveal().reveal('.showcase2', { delay:500});
+ScrollReveal().reveal('.proyects', { delay:600});
+ScrollReveal().reveal('.media', { delay:500});
+ScrollReveal().reveal('.content', { delay:500});
+ScrollReveal().reveal('.footer-links', { delay:500});
